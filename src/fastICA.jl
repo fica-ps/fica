aee0ce6a647b621ad460e86ac24b77e1ea560d96
leffiedj <leffiedj@gmail.com>
2019-03-04 19:15:11 +0000

    Commit from JuliaBox

---
+++ src/fastICA.jl
@@ -0,0 +1,17 @@
+module fastICA
+    export correlation_matrix, covariance_matrix, eigen_decomposition, whiten_data
+    using LinearAlgebra
+    using Statistics
+    
+    correlation_matrix(mat::Array{Float64 ,2})::Array{Float64 ,2} = (mat .- mean(mat,dims=1)) ./ std(mat,dims=1)   
+    
+    covariance_matrix(corrmat::Array{Float64 ,2})::Array{Float64 ,2} = corrmat |> length |> float |> n -> corrmat' * corrmat / (n  - 1.)
+
+    eigen_decomposition(mat::Array{Float64 ,2})::Eigen = mat |> correlation_matrix |> covariance_matrix |>eigen
+
+    function whiten_data(mat::Array{Float64 ,2})::Array{Float64 ,2}
+        eig = eigen_decomposition(mat)
+        return (Diagonal(eig.values .^ (-1/2)) * eig.vectors') * mat
+    end
+
+end
