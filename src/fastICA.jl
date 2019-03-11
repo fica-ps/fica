b4f8d455efcce10f674680138c024d3d24b6f293
leffiedj <leffiedj@gmail.com>
2019-03-11 17:38:04 +0000

    Commit from JuliaBox

--- src/fastICA.jl
+++ src/fastICA.jl
@@ -1,5 +1,5 @@
 module fastICA
-    export correlation_matrix, covariance_matrix, eigen_decomposition, whiten_data
+    export correlation_matrix, covariance_matrix, eigen_decomposition, sing_val_decomp, whiten
     using LinearAlgebra
     using Statistics
     
@@ -7,12 +7,19 @@ module fastICA
     
     covariance_matrix(corrmat::Array{Float64 ,2})::Array{Float64 ,2} = corrmat |> length |> float |> n -> corrmat' * corrmat / (n  - 1.)
 
-    eigen_decomposition(mat::Array{Float64 ,2})::Eigen = mat |> correlation_matrix |> covariance_matrix |>eigen
-
-    function whiten_data(mat::Array{Float64 ,2})::Array{Float64 ,2}
-        eig = eigen_decomposition(mat)
-        values = broadcast(+,  eps(0.3), eig.values)
-        return (Diagonal(values .^ (-1/2)) * eig.vectors') * mat
+    sing_val_decomp(mat::Array{Float64 ,2})::SVD = mat |> correlation_matrix |> svd
+    
+    #Returns: a whitened matrix using one of the available whitening techniques(PCA or ZCA)
+    #Receives: 
+    #the matrix to whiten 
+    #optional boolean which is used to determine the whitening technique
+    function whiten(mat::Array{Float64 ,2},pca::Bool = true)::Array{Float64 ,2}
+        U,S,_ = sing_val_decomp(mat)
+        values = broadcast(+,  eps(0.3), S)
+        if(pca)
+            return (Diagonal(values .^ (-1/2)) * U') * mat
+        end
+        return U * (Diagonal(values .^ (-1/2)) * U') * mat 
     end
 
 end
