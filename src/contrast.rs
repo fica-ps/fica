51d76ea02f9076233100b8e260a736ac7fa0c41f
adriano <adriano.halo@gmail.com>
2019-05-23 14:27:33 +0100

    updated constrast function generation to use static function pointers with O(1) search in a static function pointer array. This uses ConstrastFunctionId as index

--- src/contrast.rs
+++ src/contrast.rs
@@ -3,27 +3,23 @@ use crate::ffi::ContrastFunctionId;
 
 use arrayfire::*;
 
-pub type ContrastFunc =  Box<Fn(&Matrix) -> (Matrix, Matrix)>;
+pub type ContrastFunc = fn(&Matrix, f32) -> (Matrix, Matrix);
 
-pub fn gen_contrast_function(fid: ContrastFunctionId, alpha: f32) -> ContrastFunc {
-    
-    // TODO implement kurtosis and exp
-    match fid {
-        ContrastFunctionId::LOGCOSH => Box::new(move |m| logcosh(m, alpha)),
-        _ => unimplemented!()
-    }
-} 
+const functions: [ContrastFunc; 3] = [logcosh, logcosh, logcosh];
 
-fn logcosh(mat: &Matrix, alpha:f32) -> (Matrix, Matrix) {
-    
+pub fn get_contrast_function(fid: ContrastFunctionId) -> ContrastFunc {
+    functions[fid as usize]
+}
+
+fn logcosh(mat: &Matrix, alpha: f32) -> (Matrix, Matrix) {
     let factored_mat = mul(mat, &alpha, true);
-    
+
     // 1/alpha * log(cosh(alph * u))
     let g = log(&cosh(&factored_mat));
     let g = mul(&(1.0 / alpha), &g, true);
-    
+
     // np.tanh(alph * u)
     let dg = tanh(&factored_mat);
-    
-    (g, dg)  
+
+    (g, dg)
 }
