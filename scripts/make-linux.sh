08aa075df19663272864e6f7e17690f11fd7ceb1
Tzeroxik <adriano.halo@gmail.com>
2019-07-11 11:55:17 +0100

    added script files

---
+++ scripts/make-linux.sh
@@ -0,0 +1,8 @@
+cd ../eigen-eigen-323c052e1731/
+mkdir build   
+cd build      
+cmake ..
+cd ../../
+cmake .
+make
+
---
+++ scripts/make-win.bat
@@ -0,0 +1,7 @@
+cd ../eigen-eigen-323c052e1731/
+md build   
+cd build      
+cmake ..
+cd ../../
+cmake .
+
