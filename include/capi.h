2a71398a03221e39b776c841e7a883da63b39494
adriano <adriano.halo@gmail.com>
2019-07-12 14:54:20 +0100

    declspec(export) added

--- include/capi.h
+++ include/capi.h
@@ -13,18 +13,17 @@
 extern "C" {
 #endif
 
-
     typedef void* Matrix;
 
-    Matrix new_Matrix_c(const double* values, size_t rows, size_t cols);
+    __declspec(dllexport) Matrix new_Matrix_c(const double* values, size_t rows, size_t cols);
 
-    Matrix new_Matrix_r(const double *values, size_t rows, size_t cols);
+    __declspec(dllexport) Matrix new_Matrix_r(const double *values, size_t rows, size_t cols);
 
-    void free_Matrix(Matrix m);
+    __declspec(dllexport) void free_Matrix(Matrix m);
 
-    void print_Matrix(Matrix matrix, const char *message);
+    __declspec(dllexport) void print_Matrix(Matrix matrix, const char *message);
 
-    Matrix fast_ica(Matrix dataset, Matrix ini_weights, Matrix white_mat, ICA_Params parameters);
+    __declspec(dllexport) Matrix fast_ica(Matrix dataset, Matrix ini_weights, Matrix white_mat, ICA_Params parameters);
 
 #ifdef __cplusplus
 }
